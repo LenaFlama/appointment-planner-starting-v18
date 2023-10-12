6ee65cd8ca9cb60d3364b27638a2ed1d4cc5dc1e
Lena <lena.flama@yahoo.com>
2023-10-12 20:59:22 +0300

    'Implementing ContactForm as a form component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,14 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} required onChange={(e) => setName(e.target.value)}></input>
+        <input type="phone" pattern="^[+]40\d{9}$" value={phone} title='+407*********' required onChange={(e) => setPhone(e.target.value)}></input>
+        <input type="text" value={email}required onChange={(e) => setEmail(e.target.value)}></input>
+        <button type="submit">Save</button>
+      </form>
+    </div>
   );
 };
 

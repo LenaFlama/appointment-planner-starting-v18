7f1fe49325ca3d85c42b3e49fbce1f0a2b732f49
Lena <lena.flama@yahoo.com>
2023-10-13 20:38:51 +0300

    Implementing AppointmentForm as a stateless component

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { ContactPicker } from '../contactPicker/ContactPicker.js'
 
-const getTodayString = () => {
+const getTodayString = ({ handleSubmit }) => {
   const [month, day, year] = new Date()
     .toLocaleDateString("en-US")
     .split("/");
@@ -21,6 +22,14 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <div>
+      <form onSubmit={handleSubmit} >
+        <input type='text' value={title} required onChange={(e) => setTitle(e.target.value)}></input>
+        <input type='date' value={date} required onChange={(e) => setDate(e.target.value)}></input>
+        <input type='time' value={time} required onChange={(e) => setTime(e.target.value)}></input>
+        <ContactPicker value={contact} onChange={(e) => setContact(e.target.value)}></ContactPicker>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
   );
 };

e6df9bf229cc97bf38f788a2406de75c9b81f46e
Lena <lena.flama@yahoo.com>
2023-10-13 20:22:46 +0300

    Implementing AppointmentsPage as a stateful component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-export const Tile = (name, description) => {
+export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      <p className="tile-title">{name}</p>
+      <p className="tile-title tile">
+        {name}
+      </p>
       {Object.values(description).map((value, index) => (
         <p key={index} className="tile">
           {value}
-        </p>))}
+        </p>
+      ))}
     </div>
   );
 };
+
